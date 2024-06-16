60b3a33ec5ce652a78ddbe0dc725278a8fa780d5
Olivia <77221503+Codespace0x25@users.noreply.github.com>
2024-06-16 06:51:47 -0500

    Update Iterat.cpp

--- src/kernel/src/stdlib/Iterat.cpp
+++ src/kernel/src/stdlib/Iterat.cpp
@@ -1,7 +1,79 @@
-void iterat(int32_t *input){
-  *input++;
-}
-void revursIterat(int32_t *input){
-  *input--;
-}
-void iteraBastOnAmonr
+#ifndef ITERATOR_H
+#define ITERATOR_H
+
+template <typename T>
+class Iterator {
+public:
+    explicit Iterator(T* ptr);
+
+    T& operator*();
+    T* operator->();
+
+    Iterator& operator++();
+    Iterator operator++(int);
+
+    bool operator==(const Iterator& other) const;
+    bool operator!=(const Iterator& other) const;
+
+private:
+    T* m_ptr;
+};
+
+template <typename T>
+class ReverseIterator {
+public:
+    explicit ReverseIterator(T* ptr);
+
+    T& operator*();
+    T* operator->();
+
+    ReverseIterator& operator++();
+    ReverseIterator operator++(int);
+
+    bool operator==(const ReverseIterator& other) const;
+    bool operator!=(const ReverseIterator& other) const;
+
+private:
+    T* m_ptr;
+};
+
+template <typename T>
+class ConstIterator {
+public:
+    explicit ConstIterator(const T* ptr);
+
+    const T& operator*() const;
+    const T* operator->() const;
+
+    ConstIterator& operator++();
+    ConstIterator operator++(int);
+
+    bool operator==(const ConstIterator& other) const;
+    bool operator!=(const ConstIterator& other) const;
+
+private:
+    const T* m_ptr;
+};
+
+template <typename T>
+class ConstReverseIterator {
+public:
+    explicit ConstReverseIterator(const T* ptr);
+
+    const T& operator*() const;
+    const T* operator->() const;
+
+    ConstReverseIterator& operator++();
+    ConstReverseIterator operator++(int);
+
+    bool operator==(const ConstReverseIterator& other) const;
+    bool operator!=(const ConstReverseIterator& other) const;
+
+private:
+    const T* m_ptr;
+};
+
+#include "Iterator.tpp"
+
+#endif // ITERATOR_H
+
