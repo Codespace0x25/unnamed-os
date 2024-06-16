9975fc884f952afb7de2cd392b7c427f7a3b2019
Olivia <77221503+Codespace0x25@users.noreply.github.com>
2024-06-16 07:24:25 -0500

    Update Main.cpp

--- src/userspace/src/Main.cpp
+++ src/userspace/src/Main.cpp
@@ -1,10 +1,18 @@
-#include <stdint.h>
+#include <cstdio>
 #include <display.h>
+#include <memory>
+#include "cstddef"
 
 
-uint8_t main()
+uint8_t startUser()
 {
+  uint64_t framesRun=0;
   
+  while(true)
+  {
+    setbackground(25,1,40);
+    printf("if you are seeing this you are in user space\nand it has ran for %d",frameRun);
+    
+  }
   return 0;
-
 }
