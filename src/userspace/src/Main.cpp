31ec75832ad492bc3e6c735e9ef1053e89ccff29
Olivia <77221503+Codespace0x25@users.noreply.github.com>
2024-06-16 22:36:52 -0500

    Update Main.cpp

--- src/userspace/src/Main.cpp
+++ src/userspace/src/Main.cpp
@@ -4,16 +4,29 @@
 #include "cstddef"
 #include <stdio.h>
 
-uint8_t startUser()
+
+void drawBottomBar(void)
+{
+  for(uint16_t x=0;x<=FRAME_BUFFER_WIDTH;x++)
+  {
+    for(uint16_t y=FRAME_BUFFER_HEIGHT-10;y<=FRAME_BUFFER_HEIGHT;y++){
+      draw_pixel(x,y,RGB(80,80,80,255));
+    }
+  }
+}
+
+uint8_t startUser(...)
 {
   uint8_t framesRun=0;
   
   while(true)
   {
     if (framesRun==255){framesRun=0;}
-    setbackground(25,1,40);
+    fill_frame_buffer(RGB(25,1,40,255));
+    drawBottomBar();
     printf("if you are seeing this you are in user space\nand it has ran for %d",frameRun);
     framesRun++;
+    
   }
   return 0;
 }
